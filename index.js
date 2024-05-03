e93d414e206886b3663e40ddc205b6ed30822a03
José Ramírez <jota.rf.88@gmail.com>
2024-05-03 19:33:31 -0400

    Plot vertical chart and add customRound function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let fs = require('fs')
 
 const args = {
-	jsonFile: process.argv[2] ?? 'dataset-1'
+	jsonFile: process.argv[2] ?? 'dataset-1',
+	barWidth: 10
 }
 
 const readJsonFile = (jsonFile) => {
@@ -14,5 +15,46 @@ const readJsonFile = (jsonFile) => {
 	}
 }
 
+const plot = (chartData) => {
+	console.log('Plotting chart...')
+	const chartValues = Object.values(chartData)
+	const barLabels = Object.keys(chartData).map((label) => label.padEnd(10))
+	const maxValue = Math.max(...chartValues)
+	const normalizedMaxValue = Math.ceil(maxValue / 10) * 10
+	const linesByBar = chartValues.map((value) =>
+		customRound((value * normalizedMaxValue) / maxValue)
+	)
+
+	for (let i = normalizedMaxValue; i >= 1; i--) {
+		const lineValues = []
+
+		linesByBar.forEach((value) => {
+			const differenceBetweenValueAndI = 1 - Math.abs(value - i)
+			const shouldFormatLine = value % 1 !== 0 && i === Math.ceil(value)
+
+			if (shouldFormatLine)
+				lineValues.push(
+					'.'
+						.repeat(args.barWidth * differenceBetweenValueAndI)
+						.padEnd(args.barWidth)
+				)
+			else if (value >= i) lineValues.push('.'.repeat(args.barWidth))
+			else lineValues.push(' '.repeat(args.barWidth))
+		})
+
+		console.log(...lineValues)
+	}
+	console.log(...barLabels)
+}
+
+// 4.9 => 5
+// 4.123 => 4.1
+// 4.567 => 4.5
+const customRound = (value) => {
+	const decimalPart = value % 1
+	if (decimalPart >= 0.9) return Math.ceil(value)
+	else return Math.round(value * 10) / 10
+}
+
 const chartData = readJsonFile(args.jsonFile)
-console.log(chartData)
+plot(chartData)
